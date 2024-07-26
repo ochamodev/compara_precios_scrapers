4a1202df88c0f739d084ab29e0dffab1f6f437e1
chamodeveloper <99205618+chamodeveloper@users.noreply.github.com>
2024-07-25 19:09:27 -0600

    feat: Start adding config properties

--- src/siman/siman_config.ts
+++ src/siman/siman_config.ts
@@ -4,5 +4,25 @@
  */
 
 export interface SimanConfig {
-
+    parentUrl: string;
+    productContainerClass: string;
+    productCategories: Array<string>;
+    brandNameClass: string;
+    productLinkSelector: string;
+    noMoreItemsClass: string;
+    noMoreItemsText: string;
+    itemsAproxPerPage: number;
+    addToCartButtonText: string;
 }
+
+export const config: SimanConfig = {
+    parentUrl: "https://gt.siman.com",
+    brandNameClass: ".vtex-store-components-3-x-productBrandName",
+    addToCartButtonText: ".vtex-add-to-cart-button-0-x-buttonText",
+    productCategories: ["/celulares"],
+    productContainerClass: ".vtex-product-summary-2-x-container",
+    productLinkSelector: "a",
+    noMoreItemsClass: ".lh-copy",
+    noMoreItemsText: "No pudimos encontrar lo que estabas buscando",
+    itemsAproxPerPage: 20
+};
