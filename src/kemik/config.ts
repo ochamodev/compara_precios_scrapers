9e33359234530bdc20b9aae86ffa65c8343173de
Erick Pac <erickj.pac@gmail.com>
2024-08-01 11:48:04 -0600

    chore: init kimik bot

---
+++ src/kemik/config.ts
@@ -0,0 +1,9 @@
+export interface KemikConfigType {
+  base_url: string;
+  category_paths: string[];
+}
+
+export const config: KemikConfigType = {
+  base_url: "https://www.kemik.gt/tienda-en-linea/",
+  category_paths: ["celulares-accesorios"],
+};
