1e4992b8961550bdfed513d55373970a138c46e4
chamodeveloper <99205618+chamodeveloper@users.noreply.github.com>
2024-08-05 21:28:01 -0600

    chore: apply pr corrections

--- src/siman/siman_scraper.ts
+++ src/siman/siman_scraper.ts
@@ -7,17 +7,15 @@ import { Page, selectors } from "playwright";
 import { ProductModel } from "../common/core";
 import { getProductSku } from "./utils/scraping_utils";
 import { printProduct } from "../common/utils/scraping_utils";
+import { ScrapingArguments } from "./model/scraping_arguments";
 export class SimanScraper {
     async start() {
-
-        firefox.use(StealthPlugin());
-
         const browser = await firefox.launch({
             headless: true
         });
 
         const page = await browser.newPage();
-        let j = 1;
+        //let screenshotNumberItem = 1;
         for (const category of config.productCategories) {
             let pageNum = 1;
             while (true) {
@@ -33,7 +31,7 @@ export class SimanScraper {
                 };
                 
                 await humanLikeMouseMovement(page, 100, 100, 400, 400);
-                //await page.screenshot({path: `PageScreenshot${pageNum}.png`, fullPage: true})
+                // await page.screenshot({path: `PageScreenshot${pageNum}.png`, fullPage: true})
                 console.log("now in page ", finalUrl);
 
                 const isListEmpty = await this.isProductListEmpty(page);
@@ -58,13 +56,17 @@ export class SimanScraper {
                     });
                     return urls;
                 });
-                console.log(productUrls);
+                
+                //console.log(productUrls);
                 
                 for (const productLink of  productUrls) {
-                    j++;
-                    //await page.screenshot({path: `testResultItem${j}.png`, fullPage: true})
+                    //screenshotNumberItem++;
+                    //await page.screenshot({path: `testResultItem${screenshotNumberItem}.png`, fullPage: true})
                     await page.goto(productLink.detailUrl, {waitUntil: "networkidle"});
                     const productSku = getProductSku(productLink.detailUrl);
+
+                    const scrapingArguments: ScrapingArguments = this.getScrapingArguments(productLink, productSku)
+
                     const productModel = await page.evaluate((data) => {
                         setTimeout(() => {
                             // waiting to load description
@@ -73,13 +75,14 @@ export class SimanScraper {
                         const productImgUrl = (<HTMLImageElement>document.querySelector(`${data.selectors.imageClass}`))?.src;
                         const productName = document.querySelector('.nombreProducto')?.textContent;
                         const viewMoreBtn = (<HTMLButtonElement>document.querySelector(data.selectors.showMoreBtnClass));
-                        if (viewMoreBtn != null) {
+                        if (viewMoreBtn) {
                             viewMoreBtn.click();
                         }
                         const listPriceValue = (<HTMLElement>document.querySelector(data.selectors.listPriceValueClass))?.textContent;
                         const sellingPriceValue = (<HTMLElement>document.querySelector(data.selectors.sellingPriceValueClass))?.textContent;
                         const listItems = document.querySelector(data.selectors.specificationList);
                         let productModel = "";
+
                         for (let specification of listItems!.childNodes) {
                             if ((<HTMLElement>specification).innerText.includes('Modelo')) {
                                 let model = <HTMLElement>(<HTMLElement>specification).querySelector('.siman-m3-custom-1-x-specificationValue');
@@ -87,11 +90,12 @@ export class SimanScraper {
                                 productModel = model.innerText;
                             }
                         }
+                        
                         const product: ProductModel = {
-                            name: productName ? productName : "",
-                            brandName: brandName ? brandName : "",
+                            name: productName ?? "",
+                            brandName: brandName ?? "",
                             productDetailUrl: data.productData.detailUrl,
-                            productImageUrl: productImgUrl ? productImgUrl : "",
+                            productImageUrl: productImgUrl ?? "",
                             productModel: productModel,
                             storeSku: data.productData.sku,
                             currentPrice: null,
@@ -101,7 +105,7 @@ export class SimanScraper {
                         const priceRegex = /[^\d.-]/g;
 
                         // Not in sale
-                        if (listPriceValue == null) {
+                        if (!listPriceValue) {
                             const price = sellingPriceValue?.replace(priceRegex, '');
                             product.currentPrice = price ? parseFloat(price) : 0;
                         } else {
@@ -112,29 +116,31 @@ export class SimanScraper {
                         }
 
                         return product;
-                    }, {
-                        selectors: {
-                            brandNameClass: config.brandNameClass,
-                            showMoreBtnClass: config.showMoreBtnClass,
-                            imageClass: config.productImgUrl,
-                            sellingPriceValueClass: config.sellingPriceValueClass,
-                            listPriceValueClass: config.listPriceValueClass,
-                            specificationList: config.specificationListClass
-                        },
-                        productData: {
-                            detailUrl: productLink.detailUrl,
-                            sku: productSku
-                        } 
-                    });
-                    //const productName = await page.evaluate((brandNameClass) => document.querySelector('.nombreProducto')?.textContent, config.brandNameClass);
+                    }, scrapingArguments);
                     printProduct(productModel);
-                    //console.log(productName);
                 }
                 
                 pageNum++;
             }
         }
         console.log("Finished execution");
+    }
+
+    private getScrapingArguments(productLink: SimanProductUrlItem, productSku: string): ScrapingArguments {
+        return {
+            selectors: {
+                brandNameClass: config.brandNameClass,
+                showMoreBtnClass: config.showMoreBtnClass,
+                imageClass: config.productImgUrl,
+                sellingPriceValueClass: config.sellingPriceValueClass,
+                listPriceValueClass: config.listPriceValueClass,
+                specificationList: config.specificationListClass
+            },
+            productData: {
+                detailUrl: productLink.detailUrl,
+                sku: productSku
+            }
+        };
     }
 
     async isProductListEmpty(page: Page): Promise<boolean>{
