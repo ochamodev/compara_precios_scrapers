52958126676060f435f4b9b83bcd53ed957513f6
chamodeveloper <99205618+chamodeveloper@users.noreply.github.com>
2024-08-08 16:19:52 -0600

    feat: Move some scraping functions here

--- src/siman/utils/scraping_utils.ts
+++ src/siman/utils/scraping_utils.ts
@@ -1,3 +1,5 @@
+import { Page } from "playwright";
+import { config } from "../siman_config";
 
 export function getProductSku(link: String | null) {
     if (!link) {
@@ -6,4 +8,27 @@ export function getProductSku(link: String | null) {
     const tokens = link.split('-');
     const skuPart = tokens[tokens.length - 1].split('/')[0];
     return skuPart;
+}
+
+export async function humanLikeMouseMovement(page: Page, startX : any, startY : any, endX : any, endY : any) {
+    await page.mouse.move(startX, startY);
+    await page.waitForTimeout(100 + Math.random() * 100); // Add some randomness in the movement
+    await page.mouse.wheel(100 + Math.random(), 1000);
+    await page.mouse.move(endX, endY, { steps: 5 + Math.random() * 5 });
+}
+
+
+export async function isProductListEmpty(page: Page): Promise<boolean>{
+    const thereAreNoItems = await page.$$eval(config.noMoreItemsClass, elements => {
+        const content = elements[0].textContent
+        return content;
+    });
+    if (thereAreNoItems?.includes("No pudimos encontrar lo que estabas buscando")) {
+        return true;
+    }
+    return false;
+}
+
+export async function takeScreenshot(page: Page, pageName: string) {
+    await page.screenshot({path: pageName, fullPage: true})
 }
