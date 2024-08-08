589367e31c9a447a73eaf48ff8ff2981b1347050
chamodeveloper <99205618+chamodeveloper@users.noreply.github.com>
2024-08-08 16:19:34 -0600

    feat: Create function that logs execution time

--- src/common/utils/scraping_utils.ts
+++ src/common/utils/scraping_utils.ts
@@ -14,3 +14,10 @@ export function printProduct(product: ProductModel) {
     ----------------------------
     `);
 }
+
+
+export function logExecutionTime(ms: number) {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    return `${minutes}m ${seconds}s`;
+}
